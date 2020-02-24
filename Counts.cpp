3455027918d036c16e20b9e6d5168af2c9caa07e
Vanshika Shah <59583123+vns25@users.noreply.github.com>
2020-02-24 13:15:28 -0500

    Update Counts.cpp
    Write a C++ program that reads in lines of text from the standard input.
    
    Keep a count of the number of lines that you read.
    
    Keep track of the length of the longest line.
    
    Keep count of the number of integers in the input. Remember to check for errors before counting something as an integer. For example, 17 is an integer but 17.4 is not.
    
    When there is no more input, print out three lines of output, as follows:
    
    NUMBER OF LINES: n1
    
    LENGTH OF LONGEST LINE: n2
    
    NUMBER OF INTEGERS: n3
    
    ... where n1, n2, n3 are the counts your program kept when processing input.

--- Counts.cpp
+++ Counts.cpp
@@ -1,6 +1,19 @@
 //Vanshika Shah
 
+/**
+Direction: 
+Write a C++ program that reads in lines of text from the standard input.
+Keep a count of the number of lines that you read.
+Keep track of the length of the longest line.
+Keep count of the number of integers in the input. Remember to check for errors before counting something as an integer. For example, 17 is an integer but 17.4 is not.
+When there is no more input, print out three lines of output, as follows:
 
+NUMBER OF LINES: n1
+
+LENGTH OF LONGEST LINE: n2
+
+NUMBER OF INTEGERS: n3
+**/ 
 
 #include <iostream>
 #include <sstream>
